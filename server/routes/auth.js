1edfe861378d19de4bb51db1728c4f7da2d7eeca
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-24 20:09:27 +0530

    fix root route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { googleLogin, getusers } = require("../controller/authController");
+const { googleLogin} = require("../controller/authController");
 
 
 const router = express.Router();
