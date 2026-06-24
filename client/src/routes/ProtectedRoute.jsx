97b6ce7290024a0f41fd326c826db3ff4caf2ec6
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-24 19:10:55 +0530

    fix root route

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = () => {
   const { user } = useSelector((state) => state.auth);
 
-  if (!user) return <Navigate to="/login" replace />;
+  if (!user) return <Navigate to="https://oauth2-p9p9.onrender.com/api/login" replace />;
 
   return <Outlet />; // <-- important
 };
