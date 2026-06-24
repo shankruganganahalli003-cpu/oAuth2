134631088fda83234c8d283a70c17e07fb84cb3f
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-24 12:14:49 +0530

    fix start script

--- server/routes/worker.js
+++ server/routes/worker.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const isAuth = require("../middleware/auth");
-const { createWorker, getAllWorkers, getme, getid, update, deleteid } = require("../controller/workercontroller");
+const { createWorker, getAllWorkers, getme, getid, update, deleteid } = require("../controller/workerController");
 
 router.post("/create", isAuth, createWorker); // ✅ only authenticated users
 router.get("/getall",isAuth, getAllWorkers);
