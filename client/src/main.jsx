e6f50bd72673886e4707f338c7c675f6474f45ee
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-24 19:44:03 +0530

    fix root route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,23 +5,24 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import App from "./App.jsx";
 import { store, persistor } from "./redux/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-   
+    <GoogleOAuthProvider clientId="951092080585-0o3ojukllnmit07qroln51g2rut2j19l.apps.googleusercontent.com">
 
-<GoogleOAuthProvider clientId="951092080585-0o3ojukllnmit07qroln51g2rut2j19l.apps.googleusercontent.com">
-  <App />
-</GoogleOAuthProvider>
-          <Toaster position="top-center" reverseOrder={false} />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+            <Toaster position="top-center" reverseOrder={false} />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
